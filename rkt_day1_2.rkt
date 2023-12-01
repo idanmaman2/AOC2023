b13e51ef25a6c555cf17a3d7a88885284bf88f27
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-01 14:12:30 +0200

    format

--- rkt_day1_2.rkt
+++ rkt_day1_2.rkt
@@ -4,61 +4,30 @@
 
 (define (read-file-lines in)
   (define (parse-line line)
-    (define digits (hash 
-            "one" 1 
-            "two" 2
-            "three" 3
-            "four" 4
-            "five" 5
-            "six" 6
-            "seven" 7 
-            "eight" 8 
-            "nine" 9
-            "1" 1
-            "2" 2 
-            "3" 3
-            "4" 4
-            "5" 5
-            "6" 6
-            "7" 7
-            "8" 8
-            "9" 9
-        ))
+    (define digits
+      (hash "one" 1 "two" 2 "three" 3 "four" 4 "five" 5 "six" 6 "seven" 7 "eight" 8 "nine" 9 "1" 1 "2" 2 "3" 3 "4" 4 "5" 5 "6" 6 "7" 7 "8" 8 "9" 9))
     (define (is-number-imp line func)
-        (define (rec-is-number-imp dg)
-
-                (cond 
-                    [(empty? dg) (void)]
-                    [(zero? (string-length line)) (void) ]
-                    [(func line (car dg))
-                     (hash-ref digits (car dg))
-                    ]
-                    [
-                        else 
-                        (rec-is-number-imp  (cdr dg ))
-                    ]
-                ) 
-        )
-        (rec-is-number-imp (hash-keys digits))
-    )
+      (define (rec-is-number-imp dg)
+        (cond
+          [(empty? dg) (void)]
+          [(func line (car dg)) (hash-ref digits (car dg))]
+          [else (rec-is-number-imp (cdr dg))]))
+      (rec-is-number-imp (hash-keys digits)))
 
     (define (find-first-digit ln)
-      (let ([res (is-number-imp ln string-prefix? )])
+      (let ([res (is-number-imp ln string-prefix?)])
         (cond
           [(empty? ln) (raise 'ValueError #t)]
           [(void? res) (find-first-digit (substring ln 1))]
           [else res])))
 
     (define (find-last-digit ln)
-      (let ([res (is-number-imp ln string-suffix? )])
+      (let ([res (is-number-imp ln string-suffix?)])
         (cond
           [(empty? ln) (raise 'ValueError #t)]
-          [(void? res) (find-last-digit (substring ln 0 ( - (string-length ln) 1)))]
+          [(void? res) (find-last-digit (substring ln 0 (- (string-length ln) 1)))]
           [else res])))
-  (+ (find-last-digit line) (* (find-first-digit  line) 10))
-  
-  
-  )
+    (+ (find-last-digit line) (* (find-first-digit line) 10)))
   (let ([line (read-line in)])
     (cond
       [(eof-object? line) 0]
