ebf5930ee52fc68969fc82c898cb896db47fd34b
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-01 11:08:24 +0200

    first commit

---
+++ day1_2.py
@@ -0,0 +1,8 @@
+import re 
+import string 
+digits = ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight','nine') 
+ddigits = dict(zip(digits + tuple(string.digits[1:])  , string.digits[1:]*2 ))
+first_num  = lambda x : re.search("|".join(digits) + "|[1-9]" ,x).group(0)
+last_num =  lambda x : re.search(".*(" + "|".join(digits) + "|[1-9]).*?$" ,x).group(1)
+print(sum(map(lambda x:int(ddigits[first_num(x)]+ddigits[last_num(x)]),open("./input.txt" ,'r').readlines())) , sep="\n")
+
