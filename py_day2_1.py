42557d05a40c6a0c263da91cf860405871c6141b
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-02 23:17:00 +0200

    day2_py

---
+++ py_day2_1.py
@@ -0,0 +1,3 @@
+import re 
+rules={"red":12,"green":13,"blue":14} 
+print(sum(i[0]+1 for i in filter(lambda x :all([all(i <= mx for i,mx in zip(sg, rules.values())) for sg in x[1]]),enumerate(map(lambda x:[[int(re.search(f"(\d+) {color}",i).group(1))if re.search(f"(\d+) {color}" ,i )else 0 for color in rules]for i in x.split(";")],open("input_day2.txt",'r').read().split("\n")))))) 
