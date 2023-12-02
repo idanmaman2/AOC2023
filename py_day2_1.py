2ac45e2d05baf32b2bdc1f7d998b59f58ef04563
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-02 23:49:44 +0200

    imp

--- py_day2_1.py
+++ py_day2_1.py
@@ -1,3 +1,3 @@
 import re 
 rules={"red":12,"green":13,"blue":14} 
-print(sum(i[0] for i in filter(lambda x :all([all(i <= mx for i,mx in zip(sg, rules.values())) for sg in x[1]]),enumerate(map(lambda x:[[int((re.findall(f"(\d+) {color}",i)or[0])[0]) for color in rules]for i in x.split(";")],open("input_day2.txt",'r').read().split("\n")),1)))) 
+print(sum(i[0] for i in filter(lambda x :all([all(i <= mx for i,mx in zip(sg, rules.values())) for sg in x[1]]),enumerate(map(lambda x:[[int((re.findall(f"(\d+) {color}",i)+[0])[0]) for color in rules]for i in x.split(";")],open("input_day2.txt",'r').read().split("\n")),1)))) 
