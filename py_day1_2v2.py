3726c429d2d861b7df30b175a351d22a0d2fcd4c
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-02 23:20:03 +0200

    reformat

--- py_day1_2v2.py
+++ py_day1_2v2.py
@@ -1,5 +1,4 @@
-import re 
-import string 
+import re,string
 digits = ('one','two','three','four','five','six','seven','eight','nine') 
 ddigits = dict(zip(digits+tuple(string.digits[1:]),string.digits[1:]*2))
 print(sum(map(lambda x:int(ddigits[re.search("|".join(digits)+"|[1-9]",x).group(0)]+ddigits[re.search("|".join(i[::-1] for i in digits)+"|[1-9]",x[::-1]).group(0)[::-1]]),open("./input.txt",'r').readlines())))
