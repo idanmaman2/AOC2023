0f491e0cf3d55162b6e79abb723d711813f7aa9b
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-02 23:37:04 +0200

    imp2

--- py_day2_2.py
+++ py_day2_2.py
@@ -1,3 +1,3 @@
-import re,functools
+import re,math
 colors=("red","green","blue") 
-print(sum(i for i in map(lambda x :functools.reduce(lambda x,y:x*y,(max(vl)for vl in x[1]),1),enumerate(map(lambda x:list(zip(*[[int(re.search(f"(\d+) {color}",i).group(1))if re.search(f"(\d+) {color}",i)else 0 for color in colors]for i in x.split(";")])),open("input_day2.txt",'r').read().split("\n")))))) 
+print(sum(i for i in map(lambda x:math.prod((max(vl)for vl in x[1])),enumerate(map(lambda x:list(zip(*[[int(re.search(f"(\d+) {color}",i).group(1))if re.search(f"(\d+) {color}",i)else 0 for color in colors]for i in x.split(";")])),open("input_day2.txt",'r').read().split("\n")))))) 
