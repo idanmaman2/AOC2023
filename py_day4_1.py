069d1ddd4289083ea274de2bed1c43a84a355d7a
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-04 22:22:52 +0200

    remove re

--- py_day4_1.py
+++ py_day4_1.py
@@ -1,2 +1,2 @@
-import re,operator,math
+import operator,math
 print(sum(map(lambda x:math.floor(2**(len(operator.and_(*(set(y.split())for y in x.split("|"))))-1)),open("./inps/input_day4.txt",'r').readlines())))
