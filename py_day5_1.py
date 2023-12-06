0c38be7cb86e56e3c331b9c9a4ec907f58284e22
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-06 21:30:28 +0200

    wtf???

--- py_day5_1.py
+++ py_day5_1.py
@@ -1,3 +1,3 @@
 import re,itertools,functools,bisect
-data = open("./inps/input_day5.txt",'r').read()
-print(min(functools.reduce(lambda y,x:[i+x[1][bisect.bisect(x[0],i)-1]for i in y],map(lambda x :list(zip(*sorted(x.items(),key=lambda y:y[0]))),map(lambda x :{0:0}|dict((i[1]+i[2],0) for i in x)|dict([i[1],i[0]-i[1]]for i in x),map(lambda x : list(map(lambda x:list(map(int,x[0].split())),re.findall("((\d+\ ?)+)",x))),data.split("\n\n")[1:]))),map(int,re.findall("\d+",data.split("\n")[0])))))
+data=open("./inps/input_day5.txt",'r').read()
+print(min(functools.reduce(lambda y,x:[i+x[1][bisect.bisect(x[0],i)-1]for i in y],map(lambda x :list(zip(*sorted(x.items(),key=lambda y:y[0]))),map(lambda x:{0:0}|dict((i[1]+i[2],0)for i in x)|dict([i[1],i[0]-i[1]]for i in x),map(lambda x:list(map(lambda x:list(map(int,x[0].split())),re.findall("((\d+\ ?)+)",x))),data.split("\n\n")[1:]))),map(int,re.findall("\d+",data.split("\n")[0])))))
--- py_day5_smallnums.py
+++ py_day5_smallnums.py
@@ -1,3 +1,3 @@
 import re,itertools,functools
-data = open("./inps/input_day5.txt",'r').read()
-print(min(functools.reduce(lambda y,x:[x.get(i,i) for i in y],map(lambda x : dict(itertools.chain(*(zip(range(i[1],i[1]+i[2]),range(i[0],i[0]+i[2]))for i in x))),map(lambda x:list(map(lambda x:list(map(int,x[0].split())),re.findall("((\d+\ ?)+)",x))),data.split("\n\n")[1:])),map(int,re.findall("\d+",data.split("\n")[0])))))
+data=open("./inps/input_day5.txt",'r').read()
+print(min(functools.reduce(lambda y,x:[x.get(i,i)for i in y],map(lambda x:dict(itertools.chain(*(zip(range(i[1],i[1]+i[2]),range(i[0],i[0]+i[2]))for i in x))),map(lambda x:list(map(lambda x:list(map(int,x[0].split())),re.findall("((\d+\ ?)+)",x))),data.split("\n\n")[1:])),map(int,re.findall("\d+",data.split("\n")[0])))))
