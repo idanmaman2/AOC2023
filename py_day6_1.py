28b59c3c696fd87dea08852a0f0ab9f0a6e819e4
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-12-07 11:17:47 +0200

    imp idk

--- py_day6_1.py
+++ py_day6_1.py
@@ -1,2 +1,2 @@
-import re,math
-print(math.prod([len([0 for j in range(i+1)if(i-j)*j>d])for i,d in zip(*map(lambda i:map(int,re.findall("\d+",i)),open("./inps/input_day6.txt",'r').readlines()))]))
+import math
+print(math.prod([len([0 for j in range(i+1)if(i-j)*j>d])for i,d in zip(*map(lambda i:map(int,i.split()[1:]),open("./inps/input_day6.txt",'r')))]))
